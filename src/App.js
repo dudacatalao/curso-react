e57a6f0bdc1f5ae82594ea9c272990a3a5ade0a5
Maria Eduarda <dmelocatalao@gmail.com>
2024-02-11 21:15:33 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [], //criando array com objetos
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postResponse = fetch(
+      "https://jsonplaceholder.typicode.com/users/1/posts"
+    );
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+
+    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postAndPhotos = postsJson.map((post, index) => {
+      return {...post, cover: photosJson[index].url}
+    })
+
+    this.setState({ posts: postAndPhotos });
+  };
+
+  render() {
+    const { posts } = this.state; //construtor
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title}></img>
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
